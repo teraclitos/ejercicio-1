ff631a563ed36163c95acaacb36445ac970e8f34
teraclitos <tefans12@gmail.com>
2022-10-02 13:44:01 -0300

    agregando id

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,14 +1,28 @@
 let localSTGLista = JSON.parse(localStorage.getItem("lista")) || [];
-console.log(localSTGLista);
+let lista = document.getElementById("lista");
+
 const tarea = () => {
   let selector = document.getElementById("selector").value;
   tareasArray = [];
-  localSTGLista.forEach((element) => {
-    tareasArray.push(element);
-    console.log(element);
-  });
-  tareasArray.push({ tarea: selector });
 
-  localStorage.setItem("lista", JSON.stringify(tareasArray));
-  location.href = "http://127.0.0.1:5500/ejercicio2.html";
+  if (selector !== "Lista de tareas") {
+    localSTGLista.forEach((element) => {
+      tareasArray.push(element);
+
+      console.log(element);
+    });
+    tareasArray.push({ id: tareasArray.length + 1, tarea: selector });
+
+    localStorage.setItem("lista", JSON.stringify(tareasArray));
+    location.href = "http://127.0.0.1:5500/ejercicio2.html";
+  } else {
+    alert("Lista de tareas no es una tarea");
+  }
 };
+const RenderLista = () => {
+  lista.innerHTML = localSTGLista
+    .map((element) => `<li class="text-light"> ${element.tarea}</li>`)
+    .join("");
+};
+
+RenderLista();
